b6861cd94eecc04bd7bf4b0cfa852028696d0d56
Krishnajith V <krishnajithv@Krishnajith-Mac.local>
2025-04-02 23:04:46 +0530

    feat: inital commit portfolio

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <landing-page></landing-page>
+  </div>
+</template>
+
+<script>
+import landingPage from './components/portfolio/landingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    landingPage
+  }
+}
+</script>
+
+<style>
+
+</style>
+<style lang="scss">@import './assets/styles/theme.scss'</style>
---
+++ src/components/portfolio/mainPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="">
+      <main class="fillHeight firstpage">
+        <personal-details></personal-details>
+        <about-me></about-me>
+        <experience-details></experience-details>
+        <work-and-other></work-and-other>
+        <contact-details></contact-details>
+        <footer class="footer-class">
+          <social-media-mobile></social-media-mobile>
+          <div class="footer-div">
+              <div>Designed & Built by Krishnajith</div>
+          </div>
+        </footer>
+      </main>
+      
+  </div>
+</template>
+
+<script>
+import AboutMe from './aboutMe.vue'
+import personalDetails from './personalDetails.vue'
+import experienceDetails from './experienceDetails.vue'
+import WorkAndOther from './workAndOther.vue'
+import contactDetails from './contactDetails.vue'
+import socialMediaMobile from './socialMediaMobile.vue'
+export default {
+  components: { personalDetails, AboutMe, experienceDetails, WorkAndOther, contactDetails, socialMediaMobile },
+  name: 'mainPage'}
+</script>
+
+<style lang="scss">
+@import '../../assets/styles/mainPage.scss'; 
+@import '../../assets/styles/variables';
+</style>
